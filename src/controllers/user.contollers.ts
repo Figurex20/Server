70e865c277b24cdab3c7ca1f89699d1d55531b56
KENNETH ALONSO GOMEZ MARTINEZ <kennethggm12@gmail.com>
2023-03-20 14:12:24 -0600

    I made the inisialSetup

--- src/controllers/user.contollers.ts
+++ src/controllers/user.contollers.ts
@@ -1,6 +1,7 @@
 import { userModel } from '../models/modelUser';
 import { Response, Request } from 'express';
 import { utils } from '../middlewares/utils';
+import { roleModel } from '../models/modelRole';
 
 export class UserController {
 	static getAllUsers = async (req: Request, res: Response) => {
@@ -46,6 +47,55 @@ export class UserController {
 						.status(400)
 						.json({ status: 'something wrong with changed password', message: respond.message });
 				}
+			}
+
+			await userModel.findByIdAndUpdate(req.params.id, req.body, { new: true });
+			res.status(200).json({ status: 'User updated' });
+		} catch (error) {
+			let result = (error as DOMException).message;
+			return res.status(404).json({ message: result });
+		}
+	};
+
+	static updateUser = async (req: Request, res: Response) => {
+		try {
+			if (req.body.option === 'resetPassword') {
+				try {
+					const respond = await utils.newPasswordAmin(req.params.id);
+					return res.status(200).json(respond);
+				} catch (error) {
+					let result = (error as DOMException).message;
+					return res.status(404).json({ message: result });
+				}
+			}
+
+			if (req.body.role === 'admin') {
+				const rol = ['admin', 'moderator'];
+				const uniqueUser = await userModel.findById(req.params.id);
+				const foundRoles = await roleModel.find({ name: { $in: rol } });
+				if (!uniqueUser) return res.status(404).json({ message: 'Rol not found' });
+				uniqueUser.roles = foundRoles.map((role) => role._id);
+				await uniqueUser.save();
+			}
+
+			console.log(req.body.role);
+
+			if (req.body.role === 'moderator') {
+				const rol = ['moderator'];
+				const uniqueUser = await userModel.findById(req.params.id);
+				const foundRoles = await roleModel.find({ name: { $in: rol } });
+				if (!uniqueUser) return res.status(404).json({ message: 'Rol not found' });
+				uniqueUser.roles = foundRoles.map((role) => role._id);
+				await uniqueUser.save();
+			}
+
+			if (req.body.role === 'user') {
+				const rol = ['user'];
+				const uniqueUser = await userModel.findById(req.params.id);
+				const foundRoles = await roleModel.find({ name: { $in: rol } });
+				if (!uniqueUser) return res.status(404).json({ message: 'Rol not found' });
+				uniqueUser.roles = foundRoles.map((role) => role._id);
+				await uniqueUser.save();
 			}
 
 			await userModel.findByIdAndUpdate(req.params.id, req.body, { new: true });
